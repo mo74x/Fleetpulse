30a329741b05b6da781e1d290ebafae1597a7d5c
mohamed 3rafa <mohamed3rafa01@gmail.com>
2026-07-28 21:20:41 +0300

    feat: initialize NestJS project structure with base configuration and testing files

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, //makes config available across app
+      validationSchema: Joi.object({
+        PORT: Joi.number().default(3000),
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test')
+          .default('development'),
+        MONGO_URI: Joi.string().required(),
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().default(5432),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        REDIS_HOST: Joi.string().required(),
+        REDIS_PORT: Joi.number().default(6379),
+        RABBITMQ_URI: Joi.string().required(),
+        ELASTICSEARCH_NODE: Joi.string().required(),
+      }),
+    }),
+  ],
+})
+export class AppModule {}
